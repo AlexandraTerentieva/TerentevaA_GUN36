94857c541929ccc62c9eabb3ab25f56ca1bea859
Alexandra <Terentieva.Alexandra2020@yandex.ru>
2026-05-31 20:23:41 +0300

    sprites

--- Assets/Scripts/CellPaletteSettings.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CellPaletteSettings", menuName = "Game/Cell Palette")]
-public class CellPaletteSettings : ScriptableObject
-{
-    public Material selectedMaterial;
-    public Material canMoveMaterial;
-    public Material canAttackMaterial;
-}
